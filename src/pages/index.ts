ea600c9f598c7906b157fa157f6887e9a5934dc0
medeirosdev <guilherme.ggcm20@gmail.com>
2026-01-15 00:15:16 -0300

    refactor: export pages from barrel file

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { Dashboard } from './Dashboard';
+export { Logbook } from './Logbook';
+export { Grimoire } from './Grimoire';
+export { Snippets } from './Snippets';
+export { Profile } from './Profile';
+export { Settings } from './Settings';
